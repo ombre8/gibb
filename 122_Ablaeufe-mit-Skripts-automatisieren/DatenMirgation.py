3e4ead336a5e45cefcde1d61c1fdb373dd86e629
Lukas Grimm <ombre@ombre.ch>
2013-04-28 14:34:40 +0200

    finished Datenmigration.py

--- 122_Ablaeufe-mit-Skripts-automatisieren/DatenMirgation.py
+++ 122_Ablaeufe-mit-Skripts-automatisieren/DatenMirgation.py
@@ -26,7 +26,7 @@ with open('Data1.csv', newline='') as f:
            print('Vorname Name ' + str(rownr) + ' OK!')
          else: print('Vorname Name: '+ str(col)+' NR '+ str(rownr) + ' ERROR!')
        elif colnr == 3:
-         if re.match('^([a-zA-Z])*\s{1}[0-9]{1,}[a-z]{1}$', col):
+         if re.match('^([a-zA-Z-\'öäüéàèÖÄÜ\s])*\s{1}[0-9]+[a-z]*$', col):
            print('Strasse HNr ' + str(rownr) + ' OK!')
          else: print('Strasse HNr: '+ str(col)+' NR '+ str(rownr) + ' ERROR!')
        elif colnr == 4:
@@ -42,9 +42,9 @@ with open('Data1.csv', newline='') as f:
            print('Email ' + str(rownr) + ' OK!')
          else: print('Email: '+ str(col)+' NR ' + str(rownr) + ' ERROR!')
        elif colnr == 7:
-         if re.match('[0-9]{1-3}([.]{1}[0-9]{1-3}){3}', col):
-           print('IP-Adresse '+ str(col)+' NR ' + str(rownr) + ' OK!')
-         else: print('IP-Adresse: ' + str(rownr) + ' ERROR!')
+         if re.match('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', col):
+           print('IP-Adresse '+ str(rownr) + ' OK!')
+         else: print('IP-Adresse: '+ str(col)+' NR '  + str(rownr) + ' ERROR!')
        elif colnr == 8:
          if re.match('^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$', col): #knallharter Regex aus dem Internet (Date with leap years. Accepts '.' '-' and '/' as separators d.m.yy to dd.mm.yyyy (or d.mm.yy, etc) Ex: dd-mm-yyyy d.mm/yy dd/m.yyyy etc etc Accept 00 years also)  sonst ([1-9]|[0[1-9]|[1-2][0-9]|3[01])\.([1-9]|0[1-9]|1[0-2])\.\d{4}
            print('Datum ' + str(rownr) + ' OK!')
