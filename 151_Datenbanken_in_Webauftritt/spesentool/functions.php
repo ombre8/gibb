f85f6829b6c4df61e6a4e2b9bb3642d19b598b99
Lukas Grimm <ombre@ombre.ch>
2013-06-02 16:13:40 +0200

    inital brainstorming

--- 151_Datenbanken_in_Webauftritt/spesentool/functions.php
+++ 151_Datenbanken_in_Webauftritt/spesentool/functions.php
@@ -29,6 +29,80 @@ function closeDb($con)
 
 }
 
-// Output stuff
+// Action stuff
 
+function chooseAction(){
+	if (isset($_POST['action'])){
+		$action = $_POST['action'];
+	}
+	else if (isset($_GET['site'])){
+		$action = $_GET['site'];
+	}
+	else {
+		$action = 'table';
+	}
+	switch($action)
+	{
+		
+		case "table":
+			head();
+			table();
+			footer();
+			break;
+		case "add":
+			head;
+			add();
+			footer();
+			break;
+		default:
+			//should never happen
+	}
+}
+
+// display / html Stuff
+function head(){
+	echo "<!DOCTYPE html>
+			<html>
+			<head>
+			<title>Spesentool</title>
+			<body>
+			<h1>Spesentool</h1>
+			<p>Übersicht über alle Speseninformationen</p>";
+}
+
+function footer(){
+	echo "</body>
+			</html>";
+}
+
+function table(){
+	$con = dbConnect();
+	$query = "SELECT * FROM gibb_spesen";
+	queryDb($con, $query);
+	echo "<table>
+		<tr>
+			<td>Nummer</td>
+			<td>Mitarbeiter</td>
+			<td>Art</td>
+			<td>Betrag</td>
+			<td>Währungskurs</td>
+			<td>Datum Abrechnung</td>
+			<td>Datum Genehmigung</td>
+			<td>Genehmigt Durch</td>
+			<td>Datum Auszahlung</td>
+		</tr>";
+	foreach($query as $key => $value){
+		echo "<tr><td>".$query['id']."</td>
+		<td>".$query['mitarbeiterId']."</td>
+		<td>".$query['spesenArt']."</td>
+		<td>".$query['betrag']."</td>
+		<td>".$query['waehrungskurs']."</td>
+		<td>".$query['datumAbrechnung']."</td>
+		<td>".$query['datumGenehmigung']."</td>
+		<td>".$query['genehmigtDurch']."</td>
+		<td>".$query['datumAuszahlung']."</td>
+		</tr>";
+	}
+	echo "</table>";
+}
 ?>
